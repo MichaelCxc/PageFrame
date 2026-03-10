333bea701a6c653498d970669e7f57bfbc38367a
Chang Michael <xcchenmichael@gmail.com>
2026-03-09 22:34:26 -0400

    PageFram & PageGuard

--- main.cpp
+++ main.cpp
@@ -1,8 +1,61 @@
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
+class PageFrame;
+
+class PinGuard {
+    PageFrame* frame;
+public:
+    explicit PinGuard(PageFrame* pf);
+
+    PinGuard(const PinGuard&) = delete;
+    PinGuard& operator=(const PinGuard&) = delete;
+
+    ~PinGuard();
+};
+
+class PageFrame {
+    int pin_count = 0;
+public:
+    void pin_internal() {
+        pin_count++;
+    }
+    void unpin_internal() {
+        pin_count--;
+    }
+    int get_pin_count() const{
+        return pin_count;
+    }
+
+    PinGuard pin() {return PinGuard(this);};
+};
+
+PinGuard::PinGuard(PageFrame *pf) {
+    frame = pf;
+    if (frame != nullptr) {
+        frame->pin_internal();
+    }
+}
+
+PinGuard::~PinGuard() {
+    if (frame != nullptr) {
+        frame->unpin_internal();
+    }
+}
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    PageFrame pf;
+    cout << "init: " << pf.get_pin_count() << "\n";
+    {
+        auto g = pf.pin();
+        cout << "PinGuard after init: " << pf.get_pin_count() << "\n";
+        //PinGuard pg2(pg);
+    }
+
+    cout << "PinGuard destroyed: " << pf.get_pin_count() << "\n";
+
+
 }
